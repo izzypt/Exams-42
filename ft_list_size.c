6858695d013c2ba28aa0a4f9e6af8e85903fead2
Simão Bonvalot <73948790+izzypt@users.noreply.github.com>
2023-04-26 14:34:14 +0100

    Create ft_list_size.c

---
+++ ft_list_size.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_size.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: simao <simao@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/26 14:14:49 by simao             #+#    #+#             */
+/*   Updated: 2023/04/26 14:21:13 by simao            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+typedef struct	s_list
+{
+	struct s_list	*next;
+	void			*data;
+}	t_list;
+
+int	ft_list_size(t_list *begin_list)
+{
+	int	i;
+
+	i = 0;
+	while (begin_list)
+	{
+		begin_list = begin_list->next;
+		i++;
+	}
+	return (i);
+}
