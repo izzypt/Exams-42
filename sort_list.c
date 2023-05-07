a377f284fc132d0057be53f6f55c2e5fd9403667
Simão Bonvalot <73948790+izzypt@users.noreply.github.com>
2023-05-07 14:50:01 +0100

    Update sort_list.c

--- sort_list.c
+++ sort_list.c
@@ -1,26 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_list.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smagalha <smagalha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/07 14:28:24 by smagalha          #+#    #+#             */
+/*   Updated: 2023/05/07 14:46:23 by smagalha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
 #include "list.h"
 
-t_list *sort_list(t_list *lst, int (*cmp)(int, int))
+t_list	*sort_list(t_list *lst, int (*cmp)(int, int))
 {
-    t_list *current = lst;
-    int tmp;
+	t_list	*current;
+	t_list	*next;
+	int		tmp;
 
-    while (current != NULL)
-    {
-        t_list *next = current->next;
-        while (next != NULL)
-        {
-            if ((*cmp)(current->data, next->data) == 0)
-            {
-                tmp = current->data;
-                current->data = next->data;
-                next->data = tmp;
-            }
-            next = next->next;
-        }
-        current = current->next;
-    }
+	current = lst;
 
-    return lst;
+	while (current)
+	{
+		next = current->next;
+		while (next)
+		{
+			if (cmp(current->data, next->data) == 0)
+			{
+				tmp = current->data;
+				current->data = next->data;
+				next->data = tmp;
+			}
+			next = next->next;
+		}
+		current = current->next;
+	}
+	return (lst);
 }
 
