afd3409d57417f83a7652d5463d78f1ce7aca276
Simão Bonvalot <73948790+izzypt@users.noreply.github.com>
2023-05-08 18:07:41 +0100

    Create str_capitalizer.c

---
+++ str_capitalizer.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   str_capitalizer.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smagalha <smagalha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 17:42:15 by smagalha          #+#    #+#             */
+/*   Updated: 2023/05/08 18:05:12 by smagalha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	str_capitalizer(char *str)
+{
+	int i;
+
+    i = 0;
+	if (str[i] >= 'a' && 'z' >= str[i])
+		str[i] -= 32;
+	write(1, &str[i], 1);
+	while (str[++i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		if ((str[i] >= 'a' && str[i] <= 'z') && (str[i - 1] == ' ' || \
+		str[i - 1] == '\t'))
+			str[i] -= 32;
+		write(1, &str[i], 1);
+	}
+}
+
+int main(int argc, char **argv)
+{
+    int i;
+    
+    i = 1;
+    if (argc < 2)
+        write(1, "\n", 1);
+    else
+    {
+        while (argv[i])
+        {
+            str_capitalizer(argv[i]);
+            write(1, "\n", 1);
+            i++;
+        }
+    }
+}
