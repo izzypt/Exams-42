a98ab4a2618c62ead949f4ad32a4ae661b9f8dae
Simão Bonvalot <73948790+izzypt@users.noreply.github.com>
2023-07-05 13:30:30 +0100

    Create get_next_line.c

---
+++ rank3/get_next_line.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: simao <simao@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 13:20:24 by simao             #+#    #+#             */
+/*   Updated: 2023/07/05 13:29:14 by simao            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+char	*get_next_line(int fd)
+{
+	int		i;
+	int		rd;
+	char	character;
+	char	*buffer;
+
+	i = 0;
+	rd = 0;
+	buffer = malloc(100000);
+
+	if (BUFFER_SIZE <= 0)
+		return (NULL);
+	while ((rd = read(fd, &character, BUFFER_SIZE - BUFFER_SIZE + 1)) > 0)
+	{
+		buffer[i++] = character;
+		if (character == '\n')
+			break ;
+	}
+	buffer[i] = '\0';
+	if (rd == -1 || i == 0 || (!buffer[i - 1] && !rd))
+		return (free(buffer), NULL);
+	return (buffer);
+}
